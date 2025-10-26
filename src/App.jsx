f43e663a069266775fceb47644565343721cd564
Rafael10100 <rafaelvdtgt@gmail.com>
2025-10-25 22:21:49 -0300

    update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import RollDice from './components/RollDice'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas)
 
 function App() {
     return (
